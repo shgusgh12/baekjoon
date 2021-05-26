1a65a81b2996311b88d593f1c4bfcd9b2520cbc2
shgusgh12 <84013707+shgusgh12@users.noreply.github.com>
2021-05-26 19:41:12 +0900

    0526

---
+++ 백준/test2.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int arr1[1000];
+
+
+int result(int a) {
+	int k = 0;
+	if (a <= 10) {
+		k = a;
+	}
+	if (a < 100) {
+		k = a;
+	}
+	if (a % 100 % 10 - a % 100 / 10 == a % 100 / 10 - a / 100) {
+		k = a;
+	}
+	return k;
+}
+
+int main(void) {
+	int x;
+	int cnt = 0;
+	scanf("%d", &x);
+	for (int i = 0; i < 1000; i++) {
+		arr1[i] = 0;
+	}
+	for (int j = 1; j <= 1000; j++) {
+		arr1[result(j)] = 1;
+	}
+
+	for (int i = 1; i <= x; i++) {
+		if (arr1[i] == 1) {
+			cnt++;
+		}
+	}
+	printf("%d", cnt);
+}
+
