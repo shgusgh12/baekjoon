97374f678f40b10ef006e314950f4f20ae93435c
shgusgh12 <84013707+shgusgh12@users.noreply.github.com>
2021-05-26 19:57:24 +0900

    0526-2

--- 백준/Mai.java
+++ 백준/Mai.java
@@ -1,38 +1,19 @@
+import java.util.Scanner;
+
 
 public class Mai {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		int x;
-		int arr[] = new int[10001];
-		
-		for(x = 1; x <10000; x++) {
-				int a = result(x);
-				if( a <= 10000) {
-					arr[a] = 1;
-				}
-				
+		int y = 0;
+		x = sc.nextInt();
+		for(int i = 1 ; i < 10; i++) {
+			y = x * i;
+			System.out.printf("%d * %d = %d%n", x ,i ,y);
 		}
-		for(int i =1 ; i < 10000; i++) {
-			if(arr[i]!=1) {
-				System.out.println(i);
-			}
-		}
+	
 	}
-	public static int result(int x)  {
-		int k = x;
-		if(x >= 10000) {
-			k += x/10000; x%=10000;
-		}
-		if(x >= 1000) {
-			k+= x/1000; x%=1000;
-		}
-		if(x >= 100) {
-			k+=x/100; x%=100;
-		}
-		if(x >= 10) {
-			k+=x/10; x%=10;
-		}
-		return k+=x;
-	}
+	
 
 }
